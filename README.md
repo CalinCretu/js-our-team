1fcdf2dfdce35f723fd7f1e989c7f2469ee257d9
CalinCretu <calincretu1412@gmail.com>
2023-10-18 14:20:44 +0200

    ADD - team_members_array

---
+++ README.md
@@ -0,0 +1,4 @@
+# MEMBRI DEL TEAM
+
+- Creare un array di oggetti per rappresentare i membri del team
+ -
--- js/app.js
+++ js/app.js
@@ -1,1 +1,40 @@
 console.log('Our Team')
+
+const membri = [
+  {
+    nome: 'Wayne Barnet',
+    ruolo: 'Founder & CEO'
+    // foto: 
+  },
+  {
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor'
+    // foto: 
+  },
+  {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager'
+    // foto: 
+  },
+  {
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager'
+    // foto: 
+  },
+  {
+    nome: 'Scott Estrada',
+    ruolo: 'Developer'
+    // foto: 
+  },
+  {
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer'
+    // foto: 
+  },
+]
+for (let i = 0; i < membri.length; i++) {
+  const studenteCorrente = membri[i]
+  // console.log(studenteCorrente)
+  const member = `${studenteCorrente.nome} ${studenteCorrente.ruolo}`
+  console.log(member)
+}
